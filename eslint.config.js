5d2dd1075320a66dc90a922d1ad3a3c0da79eb1e
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-03-14 02:55:41 -0400

    Update configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,47 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import perfectionist from "eslint-plugin-perfectionist";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { FlatCompat } from '@eslint/eslintrc';
+import perfectionist from 'eslint-plugin-perfectionist';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 export default defineConfig([
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  perfectionist.configs["recommended-natural"],
-  {
-    languageOptions: {
-      sourceType: "module",
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    perfectionist.configs['recommended-natural'],
+    {
+        languageOptions: {
+            sourceType: 'module',
+        },
     },
-  },
-  {
-    rules: {
-      "perfectionist/sort-objects": "off",
-      "perfectionist/sort-object-types": "off",
+    {
+        rules: {
+            'perfectionist/sort-objects': 'off',
+            'perfectionist/sort-object-types': 'off',
+        },
     },
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-  },
-  globalIgnores([
-    "node_modules/**/*",
-    ".github/**/*",
-    ".next/**/*",
-    "public/**/*",
-    "build/**/*",
-    "*.json",
-    "*.md",
-    "*.mjs",
-    "*.js",
-    "*.css",
-    "*.xml",
-    "*.html",
-    ".gitignore",
-  ]),
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+    },
+    globalIgnores([
+        'node_modules/**/*',
+        '.github/**/*',
+        '.next/**/*',
+        'public/**/*',
+        'build/**/*',
+        '*.json',
+        '*.md',
+        '*.mjs',
+        '*.js',
+        '*.css',
+        '*.xml',
+        '*.html',
+        '*.ico',
+        '.gitignore',
+    ]),
 ]);
