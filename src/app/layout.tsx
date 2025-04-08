165a424f4a5a2cb2e9efb8ae8b19f0af07fd7ac6
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-04-07 21:39:40 -0400

    chore: add initial shadcn components (#27)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
 import './globals.css';
+import ThemeProvider from '@/components/ThemeProvider';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -25,11 +26,18 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
             </body>
         </html>
     );
