230a5a0f1b76ae69cc02a688beabccff908851c9
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-04-16 17:26:42 -0400

    chore: preview dashboard layout using shadcnui block (#51)

---
+++ src/components/nav-main.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { IconCirclePlusFilled, IconMail, type Icon } from '@tabler/icons-react';
+
+import { Button } from '@/components/ui/button';
+import {
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+export function NavMain({
+    items,
+}: {
+    items: {
+        title: string;
+        url: string;
+        icon?: Icon;
+    }[];
+}) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+                <SidebarMenu>
+                    <SidebarMenuItem className="flex items-center gap-2">
+                        <SidebarMenuButton
+                            tooltip="Quick Create"
+                            className="min-w-8 bg-neutral-900 text-neutral-50 duration-200 ease-linear hover:bg-neutral-900/90 hover:text-neutral-50 active:bg-neutral-900/90 active:text-neutral-50 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90 dark:hover:text-neutral-900 dark:active:bg-neutral-50/90 dark:active:text-neutral-900"
+                        >
+                            <IconCirclePlusFilled />
+                            <span>Quick Create</span>
+                        </SidebarMenuButton>
+                        <Button
+                            size="icon"
+                            className="size-8 group-data-[collapsible=icon]:opacity-0"
+                            variant="outline"
+                        >
+                            <IconMail />
+                            <span className="sr-only">Inbox</span>
+                        </Button>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton tooltip={item.title}>
+                                {item.icon && <item.icon />}
+                                <span>{item.title}</span>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
+}
