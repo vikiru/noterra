c1cb618900d794e1bd87cac296866fc7b5dd5976
vikiru <72267229+vikiru@users.noreply.github.com>
2025-06-26 01:31:34 -0400

    chore: define route constants

---
+++ src/constants/route.ts
@@ -0,0 +1,15 @@
+export const HOME_ROUTE = '/';
+export const DASHBOARD_ROUTE = '/dashboard';
+export const NOTES_ROUTE = '/notes';
+export const CARDS_ROUTE = '/cards';
+export const PROMPT_ROUTE = '/prompt';
+export const PROFILE_ROUTE = '/profile';
+
+export const NOTE_ROUTE = '/note';
+export const CARD_ROUTE = '/card';
+export const SHARE_ROUTE = '/share';
+
+export const SIGNUP_ROUTE = '/auth/signup';
+export const SIGNIN_ROUTE = '/auth/login';
+export const ONBOARDING_ROUTE = '/onboarding';
+export const SIGNOUT_ROUTE = '/auth/signout';
