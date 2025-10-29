d357979c7a37fc52cba4c7b9c2da5b87e0abfc88
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-10-29 18:24:40 -0400

    feat: design initial homepage (#66)
    * chore: fix incorrect import alias
    
    * feat: design initial homepage

--- src/lib/components/ui/sidebar.tsx
+++ src/lib/components/ui/sidebar.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/lib/hooks/use-mobile"
-import { cn } from "@/utils"
+import { cn } from "@/utils/cn"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
