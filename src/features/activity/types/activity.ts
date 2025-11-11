ea7f9831f1da47240afa21aa034ed31319da3f59
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-11 14:05:03 -0500

    chore: remove user activity dir

--- src/features/activity/types/activity.ts
+++
@@ -1,8 +0,0 @@
-import type z from 'zod';
-import type {
-  insertActivitySchema,
-  selectActivitySchema,
-} from '@/features/activity/schema/activitySchema';
-
-export type UserActivity = z.infer<typeof selectActivitySchema>;
-export type UserActivityCreate = z.infer<typeof insertActivitySchema>;
