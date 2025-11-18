6404e0fa51d5e34c5408678460c0db4d2082ee0c
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-18 17:07:07 -0500

    refactor: split onboarding page into separate components

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,24 +1,24 @@
 'use client';
-// import { useRouter } from 'next/navigation';
-//
-// // TODO: finish gemini prompt page (save notes and cards to db - toc update later)
-// // TODO: once this is done - check if any other backend needs work. if no, work on note zustand and server handling
-// // TODO: design notes page and each note is a card with brief metadata, flashcard count, etc. opens in new page
-// // TODO: note page shows up as preview, similar to medium articles single col layout, with edit, export as menus on top right
-// // TODO: tab to see flashcard as well + share / privacy settings
 
 import { useRouter } from 'next/navigation';
-import OnboardingForm from '@/components/forms/OnboardingForm';
+import { useEffect } from 'react';
 import { DASHBOARD_ROUTE } from '@/constants/route';
+import { OnboardingForm } from '@/lib/components/onboarding/OnboardingForm';
 import { type UserState, useUserStore } from '@/user/store/userStore';
 
-// TODO: fix this, userouter only works in client component
 export default function OnboardingPage() {
   const user = useUserStore((state: UserState) => state.user);
   const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push(DASHBOARD_ROUTE);
+    }
+  }, [user, router]);
+
   if (user) {
-    router.push(DASHBOARD_ROUTE);
-  } else {
-    return <OnboardingForm />;
+    return null;
   }
+
+  return <OnboardingForm />;
 }
