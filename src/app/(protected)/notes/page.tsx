e53d27e56b79ad37436fc4d9d230761a99dac5f9
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-19 20:19:06 -0500

    chore: cleanup components

--- src/app/(protected)/notes/page.tsx
+++ src/app/(protected)/notes/page.tsx
@@ -1,9 +1,6 @@
 import { redirect } from 'next/navigation';
 import { NotesList } from '@/features/notes/components/NotesList';
-import {
-  findNoteMetadata,
-  findNotesByUserId,
-} from '@/features/notes/data-access/notes';
+import { findNoteMetadata } from '@/features/notes/data-access/notes';
 import { getCurrentUser } from '@/lib/auth';
 import { SIGNIN_ROUTE } from '@/lib/constants/route';
 
