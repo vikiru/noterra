fa016b1f0fadc3703930ee889248499d3940645a
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-23 01:09:23 -0500

    refactor: swap to react-html-parser instead of dangerous set html for note content

--- src/features/notes/components/NoteContent.tsx
+++ src/features/notes/components/NoteContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import parse from 'html-react-parser';
 import type { RefObject } from 'react';
 import { useMermaidRender } from '@/features/notes/hooks/useMermaidRender';
 
@@ -14,9 +15,10 @@ export function NoteContent({ content, contentRef }: NoteContentProps) {
   return (
     <section
       className="max-w-none prose prose-note bg-background px-6 pb-2 pt-1 dark:prose-invert rounded-lg print:text-black print:dark:prose"
-      dangerouslySetInnerHTML={{ __html: content }}
       id="note-content"
       ref={contentRef}
-    />
+    >
+      {parse(content)}
+    </section>
   );
 }
