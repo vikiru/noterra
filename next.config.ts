05a1d40b06201a6c9ef33e0e7d230bbc9e616c56
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-11-24 17:09:31 -0500

    chore: update nextjs config to remove console logs in prod

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 export default nextConfig;
